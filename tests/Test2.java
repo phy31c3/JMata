5d2da541fa8bad00b7b6909828ab821daa3e5240
유종선(Jongsun Yu)/SC서비스개발팀/SKP <jongsun.yu@sk.com>
2017-05-24 12:00:12 +0900

    테스트 구현 완료

--- tests/Test2.java
+++ tests/Test2.java
@@ -18,7 +18,8 @@ public class Test2
 	@Test
 	public void testMain()
 	{
-		JMata.initialize(log -> System.out.println(log));
+		JMata.initialize();
+//		JMata.initialize(log -> System.out.println(log));
 		new TestMachine();
 		while (!isFinish)
 		{
@@ -40,8 +41,10 @@ public class Test2
 		int rand = (int)(Math.random() * 2);
 		switch (rand)
 		{
-		case 0: return new Noise();
-		default: return null;
+		case 0:
+			return new Noise();
+		default:
+			return null;
 		}
 	}
 	
@@ -59,46 +62,98 @@ public class Test2
 		{
 			JMata.buildMachine(TestMachine.class, builder ->
 			{
-				builder.ifPresentThenReplaceWithThis(definer ->
+				builder.ifPresentThenIgnoreThis(definer ->
 				{
 					definer.defineStartState(Start.class)
 					       .whenEnter(Start::enter)
-					       .whenInput("enum test").doThis(this::print).switchTo(EnumTest.class)
+					       .whenInput("start").switchTo(EnumTest.class)
 					       .apply()
 					
 					       .defineState(EnumTest.class)
-					       .whenEnterBy("enum test").doThis(EnumTest::enter)
+					       .whenEnterBy("start").doThis(EnumTest::enter)
 					       .whenInput(EnumSignal.SIGNAL).doThis(EnumTest::enumSignal).switchToSelf()
 					       .whenInput(EnumSignal.class).doThis(EnumTest::enumClassSignal).switchToSelf()
-					       .whenInput("enum test finished").doThis(this::print).switchTo(StringTest.class)
+					       .whenInput("enum test finished").switchTo(StringTest.class)
 					       .apply()
 					
 					       .defineState(StringTest.class)
 					       .whenEnterBy("enum test finished").doThis(StringTest::enter)
 					       .whenInput("string").doThis(StringTest::stringSignal).switchToSelf()
 					       .whenInput(String.class).doThis(StringTest::stringClassSignal).switchToSelf()
-					       .whenInput("string test finished").doThis(this::print).switchTo(CommonEnterTest.class)
+					       .whenInput("string test finished").switchTo(CommonEnterTest.class)
 					       .apply()
 					
 					       .defineState(CommonEnterTest.class)
+					       .whenEnterBy("string test finished").doThis(CommonEnterTest::enter)
+					       .whenEnterBy("string").doThis(CommonEnterTest::enterString)
+					       .whenEnterBy(EnumSignal.SIGNAL).doThis(CommonEnterTest::enterEnum)
+					       .whenEnterBy(ClassSignal.class).doThis(CommonEnterTest::enterClass)
+					       .whenInput("string").switchToSelf()
+					       .whenInput(EnumSignal.SIGNAL).switchToSelf()
+					       .whenInput(ClassSignal.class).switchToSelf()
+					       .whenInput("common enter test finished").switchTo(VoidEnterTest.class)
 					       .apply()
 					
 					       .defineState(VoidEnterTest.class)
+					       .whenEnterBy("common enter test finished").doThis(VoidEnterTest::enter)
+					       .whenEnterBy("string").doThis(VoidEnterTest::enterString)
+					       .whenEnterBy(EnumSignal.SIGNAL).doThis(VoidEnterTest::enterEnum)
+					       .whenInput("string").switchToSelf()
+					       .whenInput(EnumSignal.SIGNAL).switchToSelf()
+					       .whenInput("void enter test finished").switchTo(CommonExitTest.class)
 					       .apply()
 					
 					       .defineState(CommonExitTest.class)
+					       .whenEnterBy("void enter test finished").doThis(CommonExitTest::enter)
+					       .whenInput("string").doThis(CommonExitTest::exitString).switchToSelf()
+					       .whenInput(EnumSignal.SIGNAL).doThis(CommonExitTest::exitEnum).switchToSelf()
+					       .whenInput(ClassSignal.class).doThis(CommonExitTest::exitClass).switchToSelf()
+					       .whenInput("common exit test finished").switchTo(VoidExitTest.class)
+					       .whenExit(CommonExitTest::exit)
 					       .apply()
 					
 					       .defineState(VoidExitTest.class)
+					       .whenEnterBy("common exit test finished").doThis(VoidExitTest::enter)
+					       .whenInput("string").doThis(VoidExitTest::exitString).switchToSelf()
+					       .whenInput(EnumSignal.SIGNAL).doThis(VoidExitTest::exitEnum).switchToSelf()
+					       .whenInput("void exit test finished").switchTo(SignalsInputTest.class)
 					       .apply()
 					
 					       .defineState(SignalsInputTest.class)
+					       .whenEnterBy("void exit test finished").doThis(SignalsInputTest::enter)
+					       .whenEnterBy(Classes.class).doThis(SignalsInputTest::enterClasses)
+					       .whenEnterBy(EnumSignal.SIGNAL, EnumSignal.ENUMS).doThis(SignalsInputTest::enterEnums)
+					       .whenEnterBy("string", "strings").doThis(SignalsInputTest::enterStrings)
+					       .whenInput(ClassSignal.class, Classes.class).doThis(SignalsInputTest::exitClasses).switchToSelf()
+					       .whenInput(EnumSignal.SIGNAL, EnumSignal.ENUMS).doThis(SignalsInputTest::exitEnums).switchToSelf()
+					       .whenInput("string", "strings").doThis(SignalsInputTest::exitStrings).switchToSelf()
+					       .whenInput("signals input test finished").switchTo(RapidSwitchTest.class)
 					       .apply()
 					
 					       .defineState(RapidSwitchTest.class)
+					       .whenEnterBy("signals input test finished").doThis(RapidSwitchTest::enter)
+					       .whenEnterBy(ClassSignal.class).doThis(RapidSwitchTest::enterClass)
+					       .whenEnterBy(EnumSignal.SIGNAL).doThis(RapidSwitchTest::enterEnum)
+					       .whenEnterBy("string").doThis(RapidSwitchTest::enterString)
+					       .whenInput(ClassSignal.class).switchToSelf()
+					       .whenInput(EnumSignal.SIGNAL).switchToSelf()
+					       .whenInput("string").switchToSelf()
+					       .whenInput("rapid switch test finished").switchTo(DoNothingTest.class)
 					       .apply()
 					
 					       .defineState(DoNothingTest.class)
+					       .whenEnter(DoNothingTest::defaultEnter)
+					       .whenEnterBy("rapid switch test finished").doThis(DoNothingTest::enter)
+					       .whenEnterBy(ClassSignal.class).doNothing()
+					       .whenEnterBy(EnumSignal.SIGNAL).doNothing()
+					       .whenEnterBy("string").doNothing()
+					       .whenEnterBy("complete").doThis(DoNothingTest::enterComplete)
+					       .whenInput(ClassSignal.class).doNothing().switchToSelf()
+					       .whenInput(EnumSignal.SIGNAL).doNothing().switchToSelf()
+					       .whenInput("string").doNothing().switchToSelf()
+					       .whenInput("complete").doNothing().switchToSelf()
+					       .whenInput("do nothing test finished").doNothing().switchTo(Finish.class)
+					       .whenExit(DoNothingTest::defaultExit)
 					       .apply()
 					
 					       .defineState(Finish.class)
@@ -111,11 +166,6 @@ public class Test2
 					       .buildAndRun();
 				});
 			});
-		}
-		
-		private void print(String s)
-		{
-			System.out.println("#################### " + s);
 		}
 	}
 	
@@ -155,27 +205,21 @@ public class Test2
 	}
 	
 	@Signal
-	enum AnotherEnumSignal
-	{
-		ANOTHER_ENUM_SIGNAL,
-	}
-	
-	@Signal
 	public static class ClassSignal
 	{
-	
+		
 	}
 	
 	@Signal
 	public static class Classes
 	{
-	
+		
 	}
 	
 	@Signal
 	public static class Noise
 	{
-	
+		
 	}
 	
 	/*#########################################
@@ -186,7 +230,7 @@ public class Test2
 	{
 		static void enter()
 		{
-			input("enum test");
+			input("start");
 		}
 	}
 	
@@ -344,34 +388,34 @@ public class Test2
 		
 		static void exitClasses()
 		{
-		
+			System.out.println("17. 다중 class 입력 정의의 퇴장동작 확인");
 		}
 		
 		static void enterClasses(Classes s)
 		{
-			System.out.println("17. 다중 class 입력 정의의 전이 및 진입동작 확인");
+			System.out.println("18. 다중 class 입력 정의의 전이 및 진입동작 확인");
 			input(EnumSignal.SIGNAL);
 		}
 		
 		static void exitEnums(EnumSignal s)
 		{
-			System.out.println("18. 다중 enum 입력 정의의 퇴장동작 확인");
+			System.out.println("19. 다중 enum 입력 정의의 퇴장동작 확인");
 		}
 		
 		static void enterEnums(EnumSignal s)
 		{
-			System.out.println("19. 다중 enum 입력 정의의 전이 및 진입동작 확인");
+			System.out.println("20. 다중 enum 입력 정의의 전이 및 진입동작 확인");
 			input("strings");
 		}
 		
 		static void exitStrings(String s)
 		{
-			System.out.println("20. 다중 string 입력 정의의 퇴장동작 확인");
+			System.out.println("21. 다중 string 입력 정의의 퇴장동작 확인");
 		}
 		
 		static void enterStrings(String s)
 		{
-			System.out.println("21. 다중 string 입력 정의의 전이 및 진입동작 확인");
+			System.out.println("22. 다중 string 입력 정의의 전이 및 진입동작 확인");
 			input("signals input test finished");
 		}
 	}
@@ -379,13 +423,56 @@ public class Test2
 	@State
 	public static class RapidSwitchTest
 	{
-	
+		static Object enter()
+		{
+			return new ClassSignal();
+		}
+		
+		static Object enterClass(ClassSignal s)
+		{
+			System.out.println("23. class 입력 기민한 전이 확인");
+			return EnumSignal.SIGNAL;
+		}
+		
+		static Object enterEnum(EnumSignal s)
+		{
+			System.out.println("24. enum 입력 기민한 전이 확인");
+			return "string";
+		}
+		
+		static void enterString(String s)
+		{
+			System.out.println("25. string 입력 기민한 전이 확인");
+			input("rapid switch test finished");
+		}
 	}
 	
 	@State
 	public static class DoNothingTest
 	{
-	
+		static void enter()
+		{
+			input(new ClassSignal());
+			input(EnumSignal.SIGNAL);
+			input("string");
+			input("complete");
+		}
+		
+		static void enterComplete()
+		{
+			System.out.println("26. doNothing 확인");
+			input("do nothing test finished");
+		}
+		
+		static void defaultEnter()
+		{
+			System.out.println("실행되면 안되는 진입동작!!");
+		}
+		
+		static void defaultExit()
+		{
+			System.out.println("실행되면 안되는 퇴장동작!!");
+		}
 	}
 	
 	@State
