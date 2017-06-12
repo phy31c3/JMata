8d5ff485785da164430992729eb83d7830537ee8
JongsunYoo <phy31c3@gmail.com>
2017-06-13 00:20:19 +0900

    #55

--- src/kr/co/plasticcity/jmata/annotation/Enter.java
+++ src/kr/co/plasticcity/jmata/annotation/Enter.java
@@ -6,8 +6,8 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.SOURCE)
-@Target(ElementType.TYPE)
-public @interface Signal
+@Target(ElementType.METHOD)
+public @interface Enter
 {
-	/* empty */
+	Class<?> state() default State.class;
 }
